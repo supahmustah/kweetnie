3e5d8fb6f190aa2f409a452d8f3895dd6857f53a
Lode Van Beneden <lode.lode65@gmail.com>
2023-01-11 22:53:59 +0100

    headertext

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className='App bg-bgWhite h-screen relative w-full font-[Lato] text-accentBlack'>
+      <div className='relative top-1/4 -translate-y-1/4'>
+        <p className='text-5xl font-medium animate-moveTextDown'>
+          Lode Van Beneden
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div
+          className={`bg-accentBlack w-1/8 p-[1.5px] my-5 relative animate-expandLine left-1/2 -translate-x-1/2 opacity-0`}
+          id='movingLine'></div>
+        <p className={`text-3xl animate-moveTextUp opacity-0`}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
     </div>
   );
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,37 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['Lato']
+    },
+    extend: {
+      colors: {
+        bgWhite: '#f8f8ff',
+        accentBlack: '#242423'
+      },
+      keyframes: {
+        expandLine: {
+          '0%': { width: '0%', opacity: 1},
+          '100%': { width: '12.5%', opacity: 1}
+        },
+        moveTextDown: {
+          '0%': { transform: 'translateY(-75px)', opacity: 0},
+          '100%': { transform: 'translateY(0px)', opacity: 1}
+        },
+        moveTextUp: {
+          '0%': { transform: 'translateY(75px)', opacity: 0},
+          '100%': { transform: 'translateY(0px)', opacity: 1}
+        }
+      },
+      animation: {
+        expandLine: 'expandLine 0.65s ease-out 1.3s forwards',
+        moveTextDown: 'moveTextDown 0.65s ease-out forwards',
+        moveTextUp: 'moveTextUp 0.65s ease-out 0.65s forwards'
+      },
+      width: {
+        '1/8': "12.5%"
+      }
+    },
   },
   plugins: [],
 }
